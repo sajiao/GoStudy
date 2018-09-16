39f882ad09849906a9c2fba99817622bf3a52f2f
yijun <yijun@shinetechsoftware.com>
2018-09-16 22:32:31 +0800

    update

---
+++ container/nonrepeating.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func lengthOfNonRepeatingSubStr(s string) int{
+	last0ccurred := make(map[byte]int)
+	start :=0
+	maxLength := 0
+	for i, ch := range []byte(s){
+		if lastI, ok := last0ccurred[ch]; ok && lastI >= start{
+			start = lastI + 1
+		}
+		if i- start + 1 > maxLength{
+			maxLength = i - start + 1
+		}
+		last0ccurred[ch] = i
+	}
+	return  maxLength
+}
+
+func main(){
+	fmt.Println( lengthOfNonRepeatingSubStr("abcabcbb"))
+	fmt.Println( lengthOfNonRepeatingSubStr("bbbbbb"))
+	fmt.Println(lengthOfNonRepeatingSubStr("pwwkew"))
+	fmt.Println(lengthOfNonRepeatingSubStr("b"))
+
+}
